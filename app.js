9b71d5c7f019423fe8c2899257158fd82355ae24
Patrick <patasax22@yahoo.com>
2023-09-06 18:53:45 -0400

    HTML and styling complete, initial js

---
+++ app.js
@@ -0,0 +1,7 @@
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+const searchResults = document.getElementById("search-results");
+
+// Theme toggler elements
+const themeToggler = document.getElementById('theme-toggler')
+const body = document.body
